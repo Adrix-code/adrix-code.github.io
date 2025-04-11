d19f451dfc78f6cf8c487af6fd149f9278605f4e
Adrix-code <arrajakakdjjdjdjdjdjjf@gmail.com>
2025-04-11 09:56:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openChat() {
+  const widget = document.querySelector('chatbase-chatbot');
+  if (widget) {
+    widget.open();
+  }
+}
+
+window.embeddedChatbotConfig = {
+  chatbotId: "BjTPIMNIKIgEiQtTnz_wu",
+  domain: "www.chatbase.co",
+  openOnLoad: false
+};
